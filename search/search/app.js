daf0ac9213b570ca39a38eb6b424cc142f79b08f
Leluto Deku <hammadsiddiqui1212@gmail.com>
2023-11-23 13:40:01 +0530

    added inner html and changed button's bg

--- search/search/app.js
+++ search/search/app.js
@@ -1,3 +1,32 @@
+var products = [
+    { name: "Product 1", details: "Details 1", minBidPrice: "Price 1", time: "Time 1" },
+    { name: "Product 2", details: "Details 2", minBidPrice: "Price 2", time: "Time 2" },
+    // ... more products
+];
+var container = document.querySelector('#product-list'); 
+products.forEach(function(product) {
+    var cardHTML = `
+        <div class="col">
+            <div class="card h-100">
+                <img src="./imagecap.jpg" class="card-img-top" alt="..." />
+                <hr class="m-0" />
+                <div class="card-body">
+                    <h3 class="card-title">${product.name}</h3>
+                    <p class="card-text">${product.details}</p>
+                    <p class="min-bid-price">${product.minBidPrice}</p>
+                    <time>Time : ${product.time}</time>
+                </div>
+                <div class="card-footer">
+                    <hr class="m-0" />
+                    <button type="button" class="btn">Bid Now</button>
+                    <button type="button" class="btn">Freeze</button>
+                </div>
+            </div>
+        </div>
+    `;
+
+    container.innerHTML += cardHTML;
+});
 function mySearch() {
     var input, filter, cards, cardContainer, h5, title, i;
     input = document.getElementById("search-box");
