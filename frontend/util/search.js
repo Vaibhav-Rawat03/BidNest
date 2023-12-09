c8ef9729a5f364bb73ac5f3b748c62f384d2cab6
Leluto Deku <hammadsiddiqui1212@gmail.com>
2023-12-10 04:56:21 +0530

    most things fixed

--- frontend/util/search.js
+++ frontend/util/search.js
@@ -40,9 +40,8 @@ fetch('/getitems')
     });
   })
   .catch(error => console.error('Error fetching data:', error));
-
-
-function renderProducts(prod) {                                               //fn to create prod cards based on result from backend
+  
+  function renderProducts(prod) {
     var productList = document.getElementById('product-list');
 
     for (var index = 0; index < prod.length; index++) {
@@ -54,8 +53,7 @@ function renderProducts(prod) {                                               //
         var container = document.createElement('div');
         container.className = 'container';
 
-                                                                                    // Convert Buffer to data URL and create an object URL
-        var imageBlob = new Blob([product.image.data], { type: 'image/jpeg' });                // only works for jpeg
+        var imageBlob = new Blob([product.image.data], { type: 'image/jpeg' });
         var imageUrl = URL.createObjectURL(imageBlob);
 
         var imageCap = document.createElement('img');
@@ -64,17 +62,70 @@ function renderProducts(prod) {                                               //
         container.appendChild(imageCap);
 
         var productDetails = document.createElement('div');
-        productDetails.innerHTML = `<h2 class="product-name">${product.productname}</h2><button class="details">Show Details</button><button class="bid">Bid Now</button>`;
+        productDetails.innerHTML = `<h2 class="product-name">${product.productname}</h2>`;
         container.appendChild(productDetails);
 
+        var detailsButton = document.createElement('button');
+        detailsButton.className = 'details-btn';
+        detailsButton.textContent = 'View Details';
+        detailsButton.onclick = function() {
+            populateModal(product);
+            modal.style.display = "block";
+        };
         productCard.appendChild(container);
+        productCard.appendChild(detailsButton); // Append the button to the product card, not the container
+
         productList.appendChild(productCard);
+    }
+}
 
-        productCard.querySelector('.details').addEventListener('click', createDetailsHandler(product));
-        productCard.querySelector('.bid').addEventListener('click', createBidHandler(product));
+var modal = document.getElementById('myModal');
 
+function populateModal(product) {
+    // Get the modal details container
+    var modalDetails = document.getElementById('modal-details');
+
+    // Clear any existing details
+    modalDetails.innerHTML = '';
+
+    // Create an image element
+    var imgElement = document.createElement('img');
+    imgElement.src = `data:image/jpeg;base64,${product.image}`;
+    imgElement.alt = product.productname;
+
+    // Create a name element
+    var nameElement = document.createElement('h2');
+    nameElement.textContent = product.productname;
+
+    // Create a description element
+    var descriptionElement = document.createElement('p');
+    descriptionElement.textContent = product.description;
+	
+	var closeButton = document.createElement('button');
+    closeButton.textContent = 'Close';
+    closeButton.onclick = function() {
+        modal.style.display = "none";
+    };
+    // Append the elements to the modal details container
+    modalDetails.appendChild(imgElement);
+    modalDetails.appendChild(nameElement);
+    modalDetails.appendChild(descriptionElement);
+}
+// When the user clicks on the button, open the modal and populate it with the product details
+var btns = document.getElementsByClassName('details-btn');
+for (var i = 0; i < btns.length; i++) {
+    btns[i].onclick = function() {
+		modal.style.display = "block";
+        populateModal(product); // Pass the corresponding product object to the populateModal function
     }
 }
 
 let prod = [];                                                               //empty array will be populated khudse
 
+// Get the close button element
+var closeButton = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the close button, close the modal
+closeButton.onclick = function() {
+    modal.style.display = "none";
+}
